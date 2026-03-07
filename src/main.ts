a188c04d01b651f64d8708ab16025d5c80107295
jay <jason@mobilefringe.com>
2026-03-07 17:45:02 -0500

    web hash history

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import HomePageContainer from './views/HomePageContainer.vue'
 import InsideBeer from './views/InsideBeer.vue'
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
