69b243bee4fba987aad4428fe15d3c39e0320adc
jay <jason@mobilefringe.com>
2026-03-22 16:18:36 -0400

    reset password url fix - undo

--- src/main.ts
+++ src/main.ts
@@ -13,16 +13,6 @@ import UserLogin from "./views/UserLogin.vue"
 import ResetPassword from "./views/ResetPassword.vue"
 import axios from 'axios'
 
-const path = window.location.pathname;
-const search = window.location.search;
-
-if (
-  !window.location.hash &&
-  path.startsWith("/users/password/edit")
-) {
-  window.location.replace(`/#${path}${search}`);
-}
-
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 axios.defaults.headers.common['Accept'] = 'application/json'
@@ -57,10 +47,6 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('token')
   const isLoggedIn = !!token && token !== 'undefined'
-  
-    if (to.path === '/users/password/edit') {
-    return next()
-  }
   
   if (!to.meta.public && !isLoggedIn) {
     next('/login')
